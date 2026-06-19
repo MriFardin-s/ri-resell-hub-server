3ad18060c4888a320d17209c2ab9b1b329224bc0
rifardin <fkfardin900@gmail.com>
2026-06-19 20:46:35 +0600

    add sellerId

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express()
 const port = 9000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 require('dotenv').config();
+
+
+app.use(cors())
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -23,7 +32,40 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+
     await client.connect();
+    const database = client.db("ri-resell-hub-client");
+    const productsCollection = database.collection("products");
+
+
+    app.get('/api/products', async (req, res) => {
+      const query = {};
+      if(req.query.sellerId){
+        query.sellerId = req.query.sellerId;
+      }
+
+      if(req.query.status){
+        query.status = req.query.status;
+      }
+
+      
+      const cursor = productsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post('/api/products', async (req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
+    });
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
